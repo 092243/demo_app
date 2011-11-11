8e797de6c63b20b64bbc73997a320a7669d2af97
Priyobroto Ghosh <ghoshpriyobroto@gmail.com>
2011-11-11 12:58:05 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a70dad6a94b9ea1ec0b7fd4de57669062cd7798889c246dc5435000d1915caadaf043b4aa77038486dc948232a07849c1dc4c699e9c7f107985cdcf7163b3083'
---
+++ spec/factories.rb
@@ -0,0 +1,17 @@
+Factory.define :user do |user|
+  user.name                  "Bob"
+  user.email                 "Bob@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
+
+
+Factory.sequence :email do |n|
+  "person-#{n}@example.com"
+end
+
+
+Factory.define :micropost do |micropost|
+  micropost.content "Foo bar"
+  micropost.association :user
+end
